76395b823575b93388328750b6f1927dc8231e15
Selenium <karthickrajesh.dn@gmail.com>
2025-05-15 06:50:09 +0530

    Selenium

--- src/test/java/PageFactoryModel/LeafGroundButtonPage.java
+++ src/test/java/PageFactoryModel/LeafGroundButtonPage.java
@@ -13,4 +13,19 @@ public class LeafGroundButtonPage {
 
     @FindBy(id="j_idt88:j_idt94")
     public static WebElement positionfindButton;
+
+    @FindBy(id="j_idt88:j_idt96")
+    public static WebElement findColour;
+
+    @FindBy(id="j_idt88:j_idt98")
+    public static WebElement findSize;
+
+    @FindBy(id="j_idt88:j_idt100")
+    public static WebElement colourchange;
+
+    @FindBy(id="j_idt88:j_idt102:imageBtn")
+    public static WebElement imageButton;
+
+    @FindBy(id="j_idt88:j_idt107")
+    public static WebElement primaryButton;
 }
--- src/test/java/selenium/ButtonHandling.java
+++ src/test/java/selenium/ButtonHandling.java
@@ -1,9 +1,13 @@
 package selenium;
 
 import PageFactoryModel.LeafGroundButtonPage;
+import PageFactoryModel.LeafGroundToolBoxPage;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -45,6 +49,47 @@ public class ButtonHandling {
 
         System.out.println("X Position is " + positionX);
         System.out.println("Y Position is " + positionY);
+
+    }
+
+    @Test
+    public void findColour(){
+        String colour = LeafGroundButtonPage.findColour.getCssValue("background");
+        System.out.println("Colour is " + colour );
+    }
+
+    @Test
+    public void findSize(){
+        Dimension buttonSize = LeafGroundButtonPage.findSize.getSize();
+        int buttonHeight = buttonSize.getHeight();
+        int buttonWidth = buttonSize.getWidth();
+
+        System.out.println("Button Height is " + buttonHeight);
+        System.out.println("Button Width is " + buttonWidth);
+    }
+
+    @Test
+    public void moverOverAndConfirmColourChange(){
+        String beforeColour = LeafGroundButtonPage.colourchange.getCssValue("background-color");
+        System.out.println("beforeColour is " + beforeColour );
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(LeafGroundButtonPage.colourchange).perform();
+
+        String afterColour = LeafGroundButtonPage.colourchange.getCssValue("background-color");
+        System.out.println("afterColour is " + afterColour );
+
+        boolean colourMatching = beforeColour.equals(afterColour);
+        System.out.println("Is Colour Matching " + colourMatching);
+
+    }
+
+    @Test
+    public void clickOnImageButtonAndClickOnHiddenButton(){
+
+        LeafGroundButtonPage.imageButton.click();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", LeafGroundButtonPage.primaryButton);
 
     }
 
